5c838232948e06c1aaa06dfc436e85a6fb10f230
dand <dan.doles@gmail.com>
2014-08-12 13:24:54 -0700

    associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,4 +9,6 @@ class Listing < ActiveRecord::Base
 	end
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	validates :jobtitle, :description, :contactem, :compensation, :skills, presence: true
+
+	belongs_to :user
 end

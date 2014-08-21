8c534c43d4b4c37f28e7dd8667a7d5d46072c5cb
dand <dan.doles@gmail.com>
2014-08-21 07:27:49 -0700

    add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,29 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @users }
+    end
   end
 
-end
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @user }
+    end
+  end
 
 private
     # Use callbacks to share common setup or constraints between actions.
     def set_listing
-      @user = User.find(params[:id])
+      @user = User.find(params[:id, :image])
     end
 
   def listing_params
       params.require(:user).permit(:name, :image)
     end
-
+end

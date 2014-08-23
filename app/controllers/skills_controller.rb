d289a86db16fbe988b3e50d37c71ff3a7323f437
dand <dan.doles@gmail.com>
2014-08-22 20:57:54 -0700

    add non clickable tags to skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -72,6 +72,6 @@ class SkillsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def skill_params
-      params.require(:skill).permit(:title, :source, :description, :track, :level, :image)
+      params.require(:skill).permit(:tag_list, :title, :source, :description, :track, :level, :image)
     end
 end
